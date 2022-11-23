c6f3d0fd32eb7b310047d97e880fc60397cb9453
Mehdi Saffar <saffar.mehdi3@gmail.com>
2022-11-23 21:14:13 +0300

    setup first modal

---
+++ README.md
@@ -0,0 +1,14 @@
+# Frontend Mentor - Product preview card component solution
+Live Solution: [Product preview card component](https://fem-io-product-preview-card-component.vercel.app/)
+
+This is a solution to the [Product preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa/). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+![](./screenshot_mobile.png)
+![](./screenshot_desktop.png)
+
+## Author
+
+Mehdi Saffar
+
+- LinkedIn - [@mehdisaffar](https://www.linkedin.com/in/mehdisaffar/)
+- Frontend Mentor - [@MehdiSaffar](https://www.frontendmentor.io/profile/MehdiSaffar)
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+  // <!-- Thank you state start -->
+
+  // You selected
+  // <!-- Add rating here -->
+  // out of 5 Thank you! We appreciate you taking the time to give a rating. If you
+  // ever need more support, don’t
+  // hesitate to get in touch!
+
+  // <!-- Thank you state end -->
+</script>
+
+
+<template>
+  <main>
+    <article class="card">
+      <form>
+        <div class="card__decoration"></div>
+        <h2 class="card__title">How did we do?</h2>
+        <p>
+          Please let us know how we did with your support request. All
+          feedback is appreciated to help us
+          improve our offering!
+        </p>
+        <fieldset class="card__ratings">
+          <div class="card__rating" v-for="rating in [1, 2, 3, 4, 5]"
+            :key="rating">
+            <input type="radio" :id="`rating_${rating}`" name="rating" :value="rating" />
+            <label :for="`rating_${rating}`">{{ rating }}</label>
+          </div>
+        </fieldset>
+        <button type="submit" class="card__button">Submit</button>
+      </form>
+    </article>
+  </main>
+</template>
